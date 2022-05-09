a1ea178829ed23bdbfd72dd6389b5c73f71595d6
Maygician <maygicinc@gmail.com>
2022-05-09 18:05:26 +0900

    catproof

--- Docs/simple_context.mjs
+++ Docs/simple_context.mjs
@@ -2,19 +2,25 @@
  * use cases: 
  * 1. Simple entry access: get value of guid: global[id]
  * 2. Quadruple, to be used with proxy maps. Way to access - context.map_name[id]
- * Example: Context.A_Map[B] -> get the value of A(B)
+ * Example: Context.A_Map[B] -> get the value of A(B) (for A-relation)
+ * 
  */
  var simple_base_context={
-    //a(b) is c. value of c can 
+    //a(b) is c. value of c can //b isn't a relation.it's an id
 
     b_map:{a:'c'},//isn't stored like this, builds in runtime
-
+    d_map:{a},
 }
 global_c={//one of global contexts.
-    quadruples:['abc_id'],
-    maps:{b:'b_map'},
-    c:['','c'],
-    abc_id:['b','a','c']//id 'c' can be resolved as b(a) 
+    quadruples:['abc_id','daabe_id'],
+    // relations:[],//not needed. if there'san entry in maps - it is a relation
+    maps:{b:'b_map',d:'d_map',h:'h_map'},
+    // h_map:[],//h:d(a(a(_key_)))   h relation can be learned by following d(a(a())) route.
+    //that's just one case of neuron instances/channels/fabs with a special parser
+
+    c:['','c'],//
+    abc_id:['b','a','c'],//id 'c' can be resolved as b(a) 
+    daabe_id:[],//d(a(a(b))) is e
 }
 
 //#region interface out
@@ -26,3 +32,30 @@ bich1=new Proxy({},{
 })
 bich1.field1//
 //#endregion
+
+/**
+ * so,daabe. e:d(a(a(b_id)))
+ * quadruples:[],
+ * a_map:[b_id:'ab_id',ab_id:'aab_id']
+ * d_map:[aab_id:'e']
+ */
+
+//#region 
+/**
+ * E2:A(C), where C is A(B):
+* 1. pipe <- _resolveWord_ AC_id:
+*  here, AC_id is a followin  
+*  if(maps[c] exists) -> assuming it
+* 2. pipe <- 
+*/
+
+g2={
+    AC_id:[]
+}
+c2={_g:g2,
+maps:{},
+
+}
+
+
+//#endregion 
---
+++ temple.html/agent.js
@@ -0,0 +1,71 @@
+var agent_client_context = {}
+
+function init(init_obj){
+    let context=init_obj.c;
+    let main_activation_channel
+
+    let a={
+        main_activation_channel:(who,what)=>{
+
+        },
+
+    };
+
+    return a
+}
+
+function new_agent(){
+    var c={}
+    var __some_name={
+        activate:function(c:c){}
+    }
+}
+
+var ct1={//todo.. describe it
+    _g:global_context1,
+    neuron_instances_tuple_map:{
+        // somename:[]
+    },
+    neuron_instances_f:{
+        M1651099893264activateFbAsk_____:async function(aId,c){
+            let a = ctxt.activations_[aId];
+            //activation. [ni,ac]
+            console.log('a: ',a);
+            if(a){//fixme make generic with own context
+                // let c = ctxt.activationsContext[a[1]]
+            //    console.log('nif a0',ctxt.neuron_instances_f[a[0]],'c',c)
+                ctxt.neuron_instances_f[a[0]](a[1])
+                // ctxt.neuron_instances_f[a[0]](c)
+            }
+            else {
+                let r = rid()
+                ctxt.unsorted.ask_back_channel('M1651137773037sendMeRenderCtxt__'+r+aId)
+                //reg callback:
+                //retry()
+                ctxt.activate_these_once_full_ctxt[r]=()=>ctxt.neuron_instances_f.M1651099893264activateFbAsk_____(aId)
+            }
+        },
+    }
+}
+var global_context1={//here you can get direct value. Maps are used when you need a(b) relations (ct1)
+//quadruples are needed for "a(b) is c" see docs
+maps:{
+    neuron_instances_tuple:neuron_instances_tuple_map
+},
+quadruples:['neuron_instance_qguid_1'],
+neuron_instance_qguid_1:['neuron_instances_tuple','id22_entry'],//id22_entry is a value for neurons_instance_tuple relation 
+id22_entry:['neuron_guid1','context_guid1']
+// context_guid1:
+}
+var agent_funcs={
+    activate:function(agent,neuron_instance_name){
+        agent.c._g[neuron_instance_name];//global
+        // agent.c.neuron_instances_tuple[neuron_instance_name]
+    }
+}
+
+let client_agent=init({c:agent_client_context,});
+
+client_agent.main_activation_channel('')
+console.log(client_agent);
+
